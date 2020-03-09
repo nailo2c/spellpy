4c6a4c15c50f0d0483ed20aabf728cf1863a3d42
nailo2c <nailo2c@gmail.com>
2020-03-09 16:51:17 +0800

    change package name

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nailo",
-    version="0.1.0",
+    name="spellpy",
+    version="0.0.1",
     author="nailo2c",
     author_email="nailo2c@gmail.com",
     description="Auto parser for system raw log without human guidance",
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/nailo2c/pyspell",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
